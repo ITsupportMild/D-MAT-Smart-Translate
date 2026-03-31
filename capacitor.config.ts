9a9f4afd34d55f47eae6bbd8001e73ada0e46406
ITsupportMild <154951827+ITsupportMild@users.noreply.github.com>
2026-03-31 11:24:37 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.08fd44f3a7de4453b8142220f5019333',
+  appName: 'AI Translate',
+  webDir: 'dist',
+  server: {
+    url: 'https://08fd44f3-a7de-4453-b814-2220f5019333.lovableproject.com?forceHideBadge=true',
+    cleartext: true,
+  },
+};
+
+export default config;
