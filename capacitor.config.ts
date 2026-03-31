4cd189cad7887cfb4fe9cd6f1aa9feb92df3e3b2
ITsupportMild <154951827+ITsupportMild@users.noreply.github.com>
2026-03-31 14:26:49 +0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.hiaouan.smarttranslate', // หรือชื่อที่เฮียตั้งไว้ล่าสุด
-  appName: 'AI Translate',
+  appName: 'D-MAT Smart Translate',
   webDir: 'dist' // ตรงนี้สำคัญมาก ต้องมั่นใจว่าเป็น 'dist'
 };
 
